0c5fda2f5cb4614c74c0627bb836051d8b6e8db6
Goal651 <bugiriwilson651@gmail.com>
2026-02-13 19:24:06 +0200

    chore: Update Next.js, React, and ESLint dependencies, adjust TypeScript configuration, remove error boundary tests, and modify middleware matcher.

--- middleware.ts
+++ middleware.ts
@@ -2,12 +2,12 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
+  console.log(request.headers)
   // Add custom middleware logic here
   return NextResponse.next()
 }
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
