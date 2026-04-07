723bf9b261cf25e54bcadf4772285521d8c36177
Goal651 <bugiriwilson651@gmail.com>
2026-04-08 00:42:56 +0200

    style: remove redundant whitespace from navbar layout component

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -141,7 +141,6 @@ export function Navbar() {
                 </>
               )}
             </div>
-
             {/* Desktop Actions */}
             <div className="hidden lg:flex items-center space-x-2">
               <Button
